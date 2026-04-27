ab84c7755e08a0a89063f6fd228b82d7568469c5
yahagi-kohosya <y.yahagi.kohosya@gmail.com>
2026-04-27 14:46:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 const images = document.querySelectorAll('.image-wrap');
 const texts = document.querySelectorAll('.text');
+const overlays = document.querySelectorAll('.overlay-text');
 
 const triggerAnimation = () => {
   const triggerLine = window.innerHeight * 0.75;
 
   images.forEach(el => {
     if (el.classList.contains('active')) return;
-    const rect = el.getBoundingClientRect().top;
-    if (rect < triggerLine) {
+    if (el.getBoundingClientRect().top < triggerLine) {
       el.classList.add('active');
     }
   });
 
   texts.forEach(el => {
     if (el.classList.contains('active')) return;
-    const rect = el.getBoundingClientRect().top;
-    if (rect < triggerLine) {
+    if (el.getBoundingClientRect().top < triggerLine) {
+      el.classList.add('active');
+    }
+  });
+
+  overlays.forEach(el => {
+    if (el.classList.contains('active')) return;
+    if (el.getBoundingClientRect().top < triggerLine) {
       el.classList.add('active');
     }
   });
