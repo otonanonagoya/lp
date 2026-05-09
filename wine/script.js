19c28553140fd5838cf19abd489d2a1ba3c8de59
yahagi-kohosya <y.yahagi.kohosya@gmail.com>
2026-05-09 21:03:54 +0900

    script.js を更新

--- wine/script.js
+++ wine/script.js
@@ -1,4 +1,4 @@
- /* =========================
+/* =========================
    Fade Up Animation
 ========================= */
 
@@ -8,14 +8,14 @@ const observer = new IntersectionObserver((entries) => {
 
   entries.forEach((entry) => {
 
-    if(entry.isIntersecting){
+    if (entry.isIntersecting) {
       entry.target.classList.add('show');
     }
 
   });
 
-},{
-  threshold:0.15
+}, {
+  threshold: 0.15
 });
 
 fadeElements.forEach((el) => {
@@ -32,6 +32,7 @@ $(function () {
   $('#magazine').turn({
 
     width: 800,
+
     height: 560,
 
     autoCenter: true,
@@ -48,59 +49,36 @@ $(function () {
 
   });
 
-  // next
-  $('.archive-nav.next').on('click', function () {
-
-    $('#magazine').turn('next');
-
-  });
-
-  // prev
-  $('.archive-nav.prev').on('click', function () {
-
-    $('#magazine').turn('previous');
-
-  });
-
-});
 
   /* =========================
      Navigation
   ========================= */
 
-  const prevBtn = document.querySelector('.archive-nav.prev');
+  const prevBtn =
+    document.querySelector('.archive-nav.prev');
 
-  const nextBtn = document.querySelector('.archive-nav.next');
+  const nextBtn =
+    document.querySelector('.archive-nav.next');
 
 
+  // previous
   prevBtn.addEventListener('click', () => {
 
-    $('#magazine').turn('previous');
+    // ダミーページへ戻らない
+    if ($('#magazine').turn('page') > 2) {
+
+      $('#magazine').turn('previous');
+
+    }
 
   });
 
 
+  // next
   nextBtn.addEventListener('click', () => {
 
     $('#magazine').turn('next');
 
   });
-
-});
-const prevBtn = document.querySelector('.archive-nav.prev');
-
-const nextBtn = document.querySelector('.archive-nav.next');
-
-prevBtn.addEventListener('click', () => {
-
-  if($('#magazine').turn('page') > 2){
-    $('#magazine').turn('previous');
-  }
-
-});
-
-nextBtn.addEventListener('click', () => {
-
-  $('#magazine').turn('next');
 
 });
